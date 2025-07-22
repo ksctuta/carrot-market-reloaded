ec81cff24e51fee275ff9c24a62248c1adcba994
ksc <ksctuta@acronode.co>
2025-07-22 17:59:57 +0900

    tailwind-sample코드 중간 커밋

---
+++ app/tailwind-sample/01_darkmode/page.tsx
@@ -0,0 +1,36 @@
+export default function Home() {
+  return (
+    <main className="bg-gray-100 h-screen flex items-center justify-center py-10 dark:bg-gray-700">
+
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
+
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-gray-600 font-semibold -mx-1 dark:text-gray-400">In transit</span>
+            <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
+          </div>
+          <div className="size-12 rounded-full bg-orange-400"></div>
+        </div>
+
+        <div className="my-2 flex items-center gap-2">
+          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 transition">Today</span>
+          <span className="dark:text-gray-100">9:30-10:30</span>
+        </div>
+
+        <div className="relative">
+          <div className="absolute bg-gray-200 w-full rounded-full h-2"></div>
+          <div className="absolute bg-green-500 w-2/3 rounded-full h-2"></div>
+        </div>
+
+        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
+        </div>
+
+      </div>
+
+    </main>
+  );
+};
--- app/tailwind-sample/03_responsive1/page.tsx
+++ app/tailwind-sample/03_responsive1/page.tsx
@@ -1,18 +1,14 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center py-10">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center py-10">
 
       <div className="bg-white md:flex-row shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
         <input className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none 
           ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow 
-          placeholder:drop-shadow invalid:bg-red-100"
+          placeholder:drop-shadow"
           type="text"
-          required
           placeholder="검색어를 입력해주세요." />
-        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 
-          bg-black" >
-          Search
-        </button>
+        <button className="bg-black/50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10" >Search</button>
       </div>
 
     </main>
